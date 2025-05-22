c4b407ce24d00909f9805b683b301da4aaecb966
Llampax <llampaaxel600@gmail.com>
2025-05-21 21:12:08 -0300

    se agrego la interfaz para que se muestren sus datos y que acciones se pueden realizar con el producto

--- tp4_grupo11/src/components/ProductItem.jsx
+++ tp4_grupo11/src/components/ProductItem.jsx
@@ -27,6 +27,50 @@ const ProductItem = ({ producto, onEdit, onDelete }) => {
         setStock(producto.stock);
         setEditando(false);
     };
+    return (
+    <tr>
+      <td>{producto.id}</td>
+      <td>
+        {editando ? (
+          <input
+            value={descripcion}
+            onChange={(e) => setDescripcion(e.target.value)}
+          />
+        ) : (
+          producto.descripcion
+        )}
+      </td>
+      <td>
+        {editando ? (
+          <input
+            type="number"
+            value={precioUnitario}
+            onChange={(e) => setPrecioUnitario(Number(e.target.value))}
+          />
+        ) : (
+          producto.precioUnitario
+        )}
+      </td>
+      <td>{producto.descuento}</td>
+      <td>{producto.precioConDescuento}</td>
+      <td>{producto.stock}</td>
+      <td>
+        {editando ? (
+          <>
+            <button onClick={handleSave}>Guardar</button>
+            <button onClick={handleCancel}>Cancelar</button>
+          </>
+        ) : (
+          <button onClick={handleEditClick}>Editar</button>
+        )}
+          <button onClick={() => {  
+          if (window.confirm('¿Estás seguro de que quieres eliminar este producto?')) {
+            onDelete(producto.id); 
+          }
+        }}>Eliminar</button>
+      </td>
+    </tr>
+  );
 };
 
 export default ProductItem;
