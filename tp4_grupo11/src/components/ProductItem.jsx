b6241cf8d1e5a1f1014038a7bdda7cec00bd10ba
Isabelafk <mendoza9848248@gmail.com>
2025-05-21 21:09:38 -0300

    La funcion handleCancel cancela la edicion, restaura los valores originales del producto y al final sale del modo edicion sin guardar

--- tp4_grupo11/src/components/ProductItem.jsx
+++ tp4_grupo11/src/components/ProductItem.jsx
@@ -1,25 +1,32 @@
 import React, { useState } from "react";
 
-const ProductItem = ({ producto, onEdit, onDelete}) => {
-  const [editando, setEditando] = useState(false);
-  const [descripcion, setDescripcion] = useState(producto.descripcion);
-  const [precioUnitario, setPrecioUnitario] = useState(producto.precioUnitario);
-  const [descuento, setDescuento] = useState(producto.descuento);
-  const [stock, setStock] = useState(producto.stock);
+const ProductItem = ({ producto, onEdit, onDelete }) => {
+    const [editando, setEditando] = useState(false);
+    const [descripcion, setDescripcion] = useState(producto.descripcion);
+    const [precioUnitario, setPrecioUnitario] = useState(producto.precioUnitario);
+    const [descuento, setDescuento] = useState(producto.descuento);
+    const [stock, setStock] = useState(producto.stock);
 
-  const handleEditClick = () => setEditando(true);
-  
-  const handleSave = () => {
-    const precioConDescuento = precioUnitario * (1 - descuento / 100);
-    onEdit(producto.id, {
-      descripcion,
-      precioUnitario,
-      descuento,
-      stock,
-      precioConDescuento,
-    });
-    setEditando(false);
-  };
+    const handleEditClick = () => setEditando(true);
+
+    const handleSave = () => {
+        const precioConDescuento = precioUnitario * (1 - descuento / 100);
+        onEdit(producto.id, {
+            descripcion,
+            precioUnitario,
+            descuento,
+            stock,
+            precioConDescuento,
+        });
+        setEditando(false);
+    };
+    const handleCancel = () => {
+        setDescripcion(producto.descripcion);
+        setPrecioUnitario(producto.precioUnitario);
+        setDescuento(producto.descuento);
+        setStock(producto.stock);
+        setEditando(false);
+    };
 };
 
 export default ProductItem;
